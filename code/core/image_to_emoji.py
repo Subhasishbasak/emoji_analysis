df7a17f487b22edf266a5a777ebf05020d5775ec
subhasish <subhasish@cmi.ac.in>
2020-05-13 17:16:22 +0530

    new image_2_emoji_2 func added

--- code/core/image_to_emoji.py
+++ code/core/image_to_emoji.py
@@ -1,6 +1,9 @@
 # takes the process-able image as input outputs the list of emojis as a list of np arrays
 
 import cv2
+import statistics
+import numpy as np
+from scipy.signal import find_peaks
 
 def image_2_emoji(image):
     
@@ -33,4 +36,42 @@ def image_2_emoji(image):
     output += extract_from_half(halfed[1])
     
     return output
+
+
+def image_2_emoji_2(image):
+    img_1 = image.copy()
+    
+    kernel = np.ones((5,5),np.float32)/25
+    k = 20
+    for i in range(k):
+        image = cv2.filter2D(image,-1,kernel)
+        
+    # Row check
+    peak_len = []
+    l = np.random.uniform(low=0, high=image.shape[0], size=1000).astype(int)
+    for i in l:
+        pixel_array = image[:,:,2][i]
+        peaks, val = find_peaks(pixel_array, height=0)
+        peak_len.append(len(peaks))
+    num_emojis_row = statistics.mode(peak_len)
+    
+    # Column check
+    peak_len = []
+    l = np.random.uniform(low=0, high=image.shape[0]//2, size=1000).astype(int)
+    for i in l:
+        pixel_array = image[:,:,2][:, i]
+        peaks, val = find_peaks(pixel_array, height=0)
+        peak_len.append(len(peaks))
+    num_emojis_col = statistics.mode(peak_len)
+    
+    dim = (70*num_emojis_row,70*num_emojis_col)    
+    img_1 = cv2.resize(img_1, dim, interpolation = cv2.INTER_AREA) 
+    
+    emoji_list = []
+    for i in range(num_emojis_col):
+        for j in range(num_emojis_row):
+            temp = img_1[i*70:(i+1)*70,j*70:(j+1)*70]
+            emoji_list.append(temp)
+    
+    return emoji_list
 
